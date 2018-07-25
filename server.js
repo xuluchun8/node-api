ff45ffec4d2b177a0a58f9146af88e3db1be7e41
许路春 <xuluchun8@gmail.com>
2018-07-25 20:06:26 +0800

    connect mongodb

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 
+// 引入config
+const keys = require('./config/keys')
+
 // 测试根目录
 app.get('/', (req, res) => {
   res.json('path is / for test')
 })
 
 // 连接数据库
-mongoose.connect('mongodb://localhost:32770')
+mongoose.connect(keys.mongoURL)
   .then(() => {
     console.log('mongodb is connected')
   })
